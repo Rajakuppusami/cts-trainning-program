5c62a497761be0c7b6e5f6d6fbf18ca09daa99ca
student <student@class4-53>
2019-05-29 15:43:47 +0530

    check1

--- CollectionProblems/src/List1.java
+++ CollectionProblems/src/List1.java
@@ -7,7 +7,7 @@ public class List1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc=new Scanner(System.in);
-		int n=sc.nextInt();
+		Integer n=sc.nextInt();
 		List<Integer> li=new ArrayList<Integer>();
 		for (int i = 0; i < n; i++) {
 			li.add(sc.nextInt());
